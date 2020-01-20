2aed9f03a0d45a0a18c77300d177950dc0630bea
Jurijs Melniks <jurijs.melniks@accenture.com>
2020-01-20 15:17:22 +0200

    [fun'n'polish] Add BANNER

--- src/main/java/com/semantyca/Main.java
+++ src/main/java/com/semantyca/Main.java
@@ -1,5 +1,7 @@
 package com.semantyca;
 
+import java.util.Base64;
+
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.wildfly.swarm.Swarm;
@@ -27,7 +29,7 @@ public class Main {
         deployment.addAsResource(EmptyAsset.INSTANCE, "modules/com/h2/main/modules.xml");
         deployment.addAllDependencies();
         swarm.start().deploy(deployment);
-
+        System.out.println(new String(Base64.getDecoder().decode("ICAgICAgICAgICAgICBfX19fXyBfX19fX19fXyAgX19fX19fICAgIF8gICBfX19fX19fX19fICBfX19fX19fX19fXyAKICAgICAgICAgICAgIC8gX19fLy8gX19fXy8gIHwvICAvICAgfCAgLyB8IC8gL18gIF9fL1wgXC8gLyBfX19fLyAgIHwKICAgICAgICAgICAgIFxfXyBcLyBfXy8gLyAvfF8vIC8gL3wgfCAvICB8LyAvIC8gLyAgICBcICAvIC8gICAvIC98IHwKICAgICAgICAgICAgX19fLyAvIC9fX18vIC8gIC8gLyBfX18gfC8gL3wgIC8gLyAvICAgICAvIC8gL19fXy8gX19fIHwKICAgICAgICAgICAvX19fXy9fX19fXy9fLyAgL18vXy8gIHxfL18vIHxfLyAvXy8gICAgIC9fL1xfX19fL18vICB8X3wKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgX19fICAgIF9fX18gIF9fX18KICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAvICAgfCAgLyBfXyBcLyAgXy8KICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC8gL3wgfCAvIC9fLyAvLyAvICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgLyBfX18gfC8gX19fXy8vIC8gICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAvXy8gIHxfL18vICAgL19fXy8gICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAK")));
     }
 
 }
